1d67b2d94634a15859ac98ba873440c0ce527a25
Scully87 <scully87.ts@googlemail.com>
2014-12-30 00:15:08 +0000

    editing addPost function to take user inout

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ angular.module('flapperNews', [])
 			{title: 'post 5', upvotes: 4}
 		];
 		$scope.addPost = function() {
-			$scope.posts.push({title: 'A new post!', upvotes:0});
+			$scope.posts.push({title: $scope.title, upvotes:0});
+			$scope.title = '';
 		};
 	}]);
