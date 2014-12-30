670515e34d53b0d8dba8846d93e29f822c32ab18
Scully87 <scully87.ts@googlemail.com>
2014-12-30 03:27:07 +0000

    updated app.js- comments correctly linking to each post, incremented upvotes for comments

--- app.js
+++ app.js
@@ -1,79 +1,77 @@
-angular.module('flapperNews', ['ui.router'])
-.config([
-	'$stateProvider',
-	'$urlRouterProvider',
-	function($stateProvider, $urlRouterProvider) {
+var app = angular.module('flapperNews', ['ui.router'])
 
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: '/home.html',
-      controller: 'MainCtrl'
-    });
-  $stateProvider
-	  .state('posts', {
-		  url: '/posts/{id}',
-		  templateUrl: '/posts.html',
-		  controller: 'PostsCtrl'
-	  });
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+          .state('home', {
+              url: '/home',
+              templateUrl: '/home.html',
+              controller: 'MainCtrl'
+          })
+          .state('posts', {
+              url: '/posts/{id}',
+              templateUrl: '/posts.html',
+              controller: 'PostsCtrl'
+          });
 
-  $urlRouterProvider.otherwise('home');
+      $urlRouterProvider.otherwise('home');
+    }
+]);
 
+app.factory('posts', [function(){
+		var o = {
+			posts: []
+		};
+		return o;
 }])
 
-.factory('posts', [function(){
-	var o = {
-		posts: []
-	};
-	return o;
-}])
+app.controller('MainCtrl', ['$scope','posts',
+    function($scope, posts) {
 
-.controller
-('MainCtrl', [
-	'$scope',
-	'posts',
-	function($scope, posts){
-		$scope.posts = posts.posts;
+        // two way data binding works only on stuff in the $scope
+        $scope.posts = posts.posts;
 
-		$scope.addPost = function() {
-			if(!$scope.title || $scope.title === '') { return; } 
-			$scope.posts.push({
-				title: $scope.title,
-				link: $scope.link,
-			  upvotes:0
-			});
-			$scope.title = '';
-			$scope.link = '';
-		};
-		$scope.incrementUpvotes = function(post) {
-			post.upvotes += 1;
-		};
-		$scope.addComment = function(){
-			if($scope.body === '') { return; }
-			$scope.post.comments.push({
-			  body: $scope.body,
-			  author: 'user',
-			  upvotes: 0
-			});
-			$scope.body = '';
-		};
-		$scope.posts.push({
-		  title: $scope.title,
-		  link: $scope.link,
-		  upvotes: 0,
-		  comments: [
-		    {author: 'Joe', body: 'Cool post!', upvotes: 0},
-		    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-		  ]
-		});
+        $scope.addPost = function() {
+            if (!$scope.title || $scope.title === '') { return; }
+            $scope.posts.push({
+                title: $scope.title,
+                link: $scope.link,
+                upvotes :  0 ,
+                comments: [
+                    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+                ]
+            });
+            $scope.title = '';
+            $scope.link = '';
+        }
 
-	('PostsCtrl', [
-		'$scope',
-		'$stateParams',
-		'posts',
-		function($scope, $stateParams, posts){
-			$scope.posts = posts.posts[$stateParams.id];
-	}]);
+        $scope.incrementUpvotes = function(post) {
+            post.upvotes += 1;
+        };
+    }
+]);
 
-}]);
+app.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts){
+        $scope.post = posts.posts[$stateParams.id];
 
+        $scope.addComment = function(){
+            if($scope.body === '') { return; }
+            $scope.post.comments.push({
+                body: $scope.body,
+                author: 'user',
+                upvotes :  0
+            });
+            $scope.body = '';
+        };
+        $scope.incrementUpvotes = function(post) {
+            post.upvotes += 1;
+        };
+    }
+]);
